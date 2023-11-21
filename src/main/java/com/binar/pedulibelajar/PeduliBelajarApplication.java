80970e8029de73fb0a76403787479ddd35b5cac1
fazarhdyt <m.fajar184@gmail.com>
2023-11-21 16:23:32 +0700

    init

---
+++ src/main/java/com/binar/pedulibelajar/PeduliBelajarApplication.java
@@ -0,0 +1,13 @@
+package com.binar.pedulibelajar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeduliBelajarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeduliBelajarApplication.class, args);
+	}
+
+}
