2cb3f2c3218718b966bd210fe22893bb3dd3fb7d
Tsabit Gholib <tsabit1502@gmail.com>
2023-11-27 20:45:23 +0700

    Create Course Controller

--- src/main/java/com/binar/pedulibelajar/Controller/CourseController.java
+++ src/main/java/com/binar/pedulibelajar/Controller/CourseController.java
@@ -1,21 +1,43 @@
 package com.binar.pedulibelajar.Controller;
 
-import com.binar.pedulibelajar.Model.Request.CourseRequest;
-import com.binar.pedulibelajar.Model.Response.CourseResponse;
+import com.binar.pedulibelajar.dto.Request.CourseRequest;
+import com.binar.pedulibelajar.dto.Response.CourseResponse;
+import com.binar.pedulibelajar.Service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-public interface CourseController {
+@RestController
+@RequestMapping("/api/course")
+public class CourseController {
+    @Autowired
+    private CourseService courseService;
     @GetMapping("/courses")
-    ResponseEntity<List<CourseResponse>> getAllCourse();
-    @GetMapping("/courses/{id}")
-    ResponseEntity<CourseResponse> getCourseById(@PathVariable String id);
-    @PostMapping("/course")
-    ResponseEntity<CourseResponse> createCourse(@RequestBody CourseRequest courseRequest);
-    @PutMapping("/course/{id}")
-    ResponseEntity<CourseResponse> updateCourse(@PathVariable String id, @RequestBody CourseRequest courseRequest);
-    @DeleteMapping("/courses/{id}")
-    ResponseEntity<Void> deleteCourse(@PathVariable String id);
+    public ResponseEntity<List<CourseResponse>> getAllCourses(){
+        return ResponseEntity.ok(courseService.getAllCourses());
+    }
+
+    @GetMapping("/{courseCode}")
+    public ResponseEntity<CourseResponse> getCourseByCourseCode(@PathVariable String courseCode) {
+        return ResponseEntity.ok(courseService.getCourseByCourseCode(courseCode));
+    }
+
+    @PostMapping
+    public ResponseEntity<CourseResponse> createCourse(@RequestBody CourseRequest courseRequest) {
+        return ResponseEntity.ok(courseService.createCourse(courseRequest));
+    }
+
+    @PutMapping("/{courseCode}")
+    public ResponseEntity<CourseResponse> updateCourse(@PathVariable String courseCode, @RequestBody CourseRequest courseRequest) {
+        return ResponseEntity.ok(courseService.updateCourse(courseCode, courseRequest));
+    }
+
+    @DeleteMapping("/{courseCode}")
+    public ResponseEntity deleteCourse(@PathVariable String courseCode) {
+        courseService.deleteCourse(courseCode);
+        return ResponseEntity.ok(null);
+    }
+
 }
