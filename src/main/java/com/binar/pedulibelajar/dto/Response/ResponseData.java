aef67eb20fe2f8008bf3d1b47b36e8cc60717f7e
fazarhdyt <m.fajar184@gmail.com>
2023-11-29 10:27:08 +0700

    add response data to handle response in controller

---
+++ src/main/java/com/binar/pedulibelajar/dto/Response/ResponseData.java
@@ -0,0 +1,26 @@
+package com.binar.pedulibelajar.dto.Response;
+
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
+public class ResponseData {
+
+    public static ResponseEntity<Object> statusResponse(Object data, HttpStatus status, String message) {
+        Map<String, Object> bodyResponse = new HashMap<>();
+        if (data != null) {
+            bodyResponse.put("data", data);
+        }
+        bodyResponse.put("status", status.value());
+        bodyResponse.put("message", message);
+        return ResponseEntity.status(status).body(bodyResponse);
+    }
+
+    public static ResponseEntity<Object> internalServerError(String message) {
+        return statusResponse(null, HttpStatus.INTERNAL_SERVER_ERROR, message);
+    }
+}
