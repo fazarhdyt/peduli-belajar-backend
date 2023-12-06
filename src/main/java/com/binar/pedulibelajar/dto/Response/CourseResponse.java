d8fe2ec75ca7cafbcff07135d38e1219c4ecf0e3
Tsabit Gholib <tsabit1502@gmail.com>
2023-12-06 15:26:24 +0700

    Refactor CourseResponse

--- src/main/java/com/binar/pedulibelajar/dto/Response/CourseResponse.java
+++ src/main/java/com/binar/pedulibelajar/dto/Response/CourseResponse.java
@@ -1,20 +1,18 @@
-package com.binar.pedulibelajar.dto.Response;
+package com.binar.pedulibelajar.dto.response;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class CourseResponse {
-    private int code;
-    private String status;
-    private CourseData data;
-
-    @Data
-    public static class CourseData {
-        private String name;
-        private String category;
-        private String courseCode;
-        private String type;
-        private String level;
-        private double price;
-    }
+    private String name;
+    private String courseCode;
+    private String category;
+    private String type;
+    private String level;
+    private double price;
+    private String description;
+    private String author;
+    private List<ChapterResponse> chapter;
 }
