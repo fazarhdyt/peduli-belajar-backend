d2613b5b3759c236640aab2003f6322c86f8b5bb
Tsabit Gholib <tsabit1502@gmail.com>
2023-12-06 15:34:02 +0700

    add security

---
+++ src/main/java/com/binar/pedulibelajar/security/jwt/JwtUtils.java
@@ -0,0 +1,50 @@
+package com.binar.pedulibelajar.security.jwt;
+
+import com.binar.pedulibelajar.model.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+@Slf4j
+public class JwtUtils {
+
+    @Value("${pedulibelajar.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${pedulibelajar.jwtExpirationMs}")
+    private int jwtExpirationMs;
+
+    public String generateJwtToken(Authentication authentication) {
+        User userPrincipal = (User) authentication.getPrincipal();
+        return Jwts.builder()
+                .setSubject(userPrincipal.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    public String getUsernameFromJwtToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(jwtSecret)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateJwtToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            log.error("Cannot validate jwt token: {}", e.getMessage());
+        }
+        return false;
+    }
+}
