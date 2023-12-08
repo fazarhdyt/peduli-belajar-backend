dc8618dd122b7c02905f8c65b1cb02cfa5446297
Tsabit Gholib <tsabit1502@gmail.com>
2023-12-08 20:57:37 +0700

    delete materialRepository

--- src/main/java/com/binar/pedulibelajar/repository/MaterialRepository.java
+++
@@ -1,8 +0,0 @@
-package com.binar.pedulibelajar.repository;
-
-import com.binar.pedulibelajar.model.MaterialType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MaterialRepository extends JpaRepository<MaterialType, String> {
-    MaterialType findByMaterialTypeName(String materialTypeName);
-}
