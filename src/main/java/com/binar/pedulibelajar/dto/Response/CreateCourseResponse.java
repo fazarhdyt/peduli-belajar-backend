6478f3dd92da96bd26e6cf8a17e3c3c1383b0a8b
Tsabit Gholib <tsabit1502@gmail.com>
2023-12-08 20:54:15 +0700

    refactor name to title, author to teacher

--- src/main/java/com/binar/pedulibelajar/dto/Response/CreateCourseResponse.java
+++ src/main/java/com/binar/pedulibelajar/dto/Response/CreateCourseResponse.java
@@ -5,12 +5,12 @@ import lombok.Data;
 @Data
 public class CreateCourseResponse {
 
-    private String name;
+    private String title;
     private String courseCode;
     private String category;
     private String type;
     private String level;
     private Long price;
     private String description;
-    private String author;
+    private String teacher;
 }
