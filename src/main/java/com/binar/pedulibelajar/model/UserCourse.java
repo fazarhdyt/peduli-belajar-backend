805f4654ff5a44ac38226385a7a4392654feea71
fazarhdyt <m.fajar184@gmail.com>
2023-12-09 19:08:16 +0700

    feat(user): add model user course

---
+++ src/main/java/com/binar/pedulibelajar/model/UserCourse.java
@@ -0,0 +1,32 @@
+package com.binar.pedulibelajar.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class UserCourse {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    private String chapter;
+    private String subject;
+    private boolean done;
+}
