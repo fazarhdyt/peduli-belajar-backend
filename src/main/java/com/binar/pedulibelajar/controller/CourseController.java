713a72ad23440c2094046b71a26a473c3c4fe26d
fazarhdyt <m.fajar184@gmail.com>
2023-12-11 22:50:32 +0700

    fix(security): modified config for cors

--- src/main/java/com/binar/pedulibelajar/controller/CourseController.java
+++ src/main/java/com/binar/pedulibelajar/controller/CourseController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-//@CrossOrigin("*")
+@CrossOrigin("https://localhost:5173")
 @RestController
 @RequestMapping("/api")
 public class CourseController {
