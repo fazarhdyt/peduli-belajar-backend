6bd7c1575cbbbfbff523d3880fdacad0fdf71010
fazarhdyt <m.fajar184@gmail.com>
2023-12-11 21:34:28 +0700

    fix(security): annoted @config and @enablewebmvc

--- src/main/java/com/binar/pedulibelajar/swagger/WebConfigCors.java
+++ src/main/java/com/binar/pedulibelajar/swagger/WebConfigCors.java
@@ -1,8 +1,12 @@
 package com.binar.pedulibelajar.swagger;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@Configuration
+@EnableWebMvc
 public class WebConfigCors extends WebMvcConfigurerAdapter {
 
             @Override
