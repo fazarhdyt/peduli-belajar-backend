9e75a28cd16d6cc6e02501a8879eaead1838621f
fazarhdyt <m.fajar184@gmail.com>
2023-12-13 23:55:18 +0700

    feat(model): create enumeration

---
+++ src/main/java/com/binar/pedulibelajar/enumeration/CourseCategory.java
@@ -0,0 +1,9 @@
+package com.binar.pedulibelajar.enumeration;
+
+public enum CourseCategory {
+    UIUX_DESIGN,
+    WEB_DEVELOPMENT,
+    ANDROID_DEVELOPMENT,
+    IOS_DEVELOPMENT,
+    PRODUCT_MANAGEMENT
+}
---
+++ src/main/java/com/binar/pedulibelajar/enumeration/PaymentMethod.java
@@ -0,0 +1,6 @@
+package com.binar.pedulibelajar.enumeration;
+
+public enum PaymentMethod {
+    BANK_TRANSFER,
+    CREDIT_CARD
+}
