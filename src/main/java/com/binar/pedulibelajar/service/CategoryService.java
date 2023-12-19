4883e314cb834b2b0a734375a2932e653509b54e
fazarhdyt <m.fajar184@gmail.com>
2023-12-19 23:31:11 +0700

    feat(category): create api get categories

--- src/main/java/com/binar/pedulibelajar/service/CategoryService.java
+++ src/main/java/com/binar/pedulibelajar/service/CategoryService.java
@@ -1,45 +1,10 @@
 package com.binar.pedulibelajar.service;
 
-import com.binar.pedulibelajar.model.Category;
-import com.binar.pedulibelajar.repository.CategoryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 
-@Service
-public class CategoryService {
+import com.binar.pedulibelajar.dto.response.CategoryResponse;
 
-    private final CategoryRepository categoryRepository;
+public interface CategoryService {
 
-    @Autowired
-    public CategoryService(CategoryRepository categoryRepository) {
-        this.categoryRepository = categoryRepository;
-    }
-
-    public List<Category> getAllCategories() {
-        return categoryRepository.findAll();
-    }
-
-    public Category getCategoryById(Long id) {
-        return categoryRepository.findById(id).orElse(null);
-    }
-
-    public Category createCategory(Category category) {
-        return categoryRepository.save(category);
-    }
-
-    public Category updateCategory(Long id, Category category) {
-        Category existingCategory = categoryRepository.findById(id).orElse(null);
-        if (existingCategory != null) {
-            existingCategory.setName(category.getName());
-            existingCategory.setDescription(category.getDescription());
-            return categoryRepository.save(existingCategory);
-        }
-        return null;
-    }
-
-    public void deleteCategory(Long id) {
-        categoryRepository.deleteById(id);
-    }
+    List<CategoryResponse> getCategories();
 }
